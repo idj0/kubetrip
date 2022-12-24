50af54d055943ca4b9228db8797bdebc3f68dc78
Vian <cocatrip@proton.me>
2022-12-24 15:56:07 +0700

    feat: add test manifest

--- test/test-create.sh
+++
@@ -1,2 +0,0 @@
-kubectl create deployment hello-node --image=k8s.gcr.io/echoserver:1.4
-kubectl expose deployment hello-node --type=LoadBalancer --port=8080
--- test/test-delete.sh
+++
@@ -1,2 +0,0 @@
-kubectl delete deployment hello-node
-kubectl delete service hello-node
