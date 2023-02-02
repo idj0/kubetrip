d41b6b70b95b95534635049e53d99cf172af083a
Vian <cocatrip@proton.me>
2023-02-03 03:53:26 +0700

    feat: add reconnect script

--- scripts/reconnect.sh
+++ scripts/reconnect.sh
@@ -1,41 +1,69 @@
 #!/usr/bin/env bash
 
 function log() {
-	echo "$(TZ='Asia/Jakarta' date): ${1}"
+    echo "$(TZ='Asia/Jakarta' date): ${1}"
 }
 
 function reconnect() {
-	if ping -c 1 1.1.1.1 &> /dev/null
-	then
-		log "connected"
-	else
-		log "disconnected"
+    log "waiting for available ssid"
+    while [[ $(nmcli device wifi list --rescan yes | grep -c Cita) == 0 ]]; do
+        sleep 0.1
+    done
 
-		log "rescanning wifi"
-		nmcli device wifi rescan
+    log "restarting network-manager"
+    sudo systemctl restart snap.network-manager.networkmanager.service
+    while [[ $(systemctl status snap.network-manager.networkmanager.service | grep -c 'active (running)') == 0 ]]; do
+        sleep 0.1
+    done
 
-		log "waiting for available ssid"
-		while [[ $(nmcli device wifi list | grep Cita | wc -l) == 0 ]]
-		do
-			sleep 1
-		done
+    log "connecting to Cita or Cita_Ext"
+    {
+        log "trying Cita_Ext"
+        nmcli connection up netplan-wlp3s0-Cita_Ext &>/dev/null
+        wait
+        } || {
+        log "trying Cita"
+        nmcli connection up netplan-wlp3s0-Cita &>/dev/null
+        wait
+        } || {
+        err=$?
+        log "failed with status code ${err}"
+        return "$err"
+    }
 
-		log "connecting to Cita or Cita_Ext"
-		{
-			log "trying Cita"
-			nmcli device wifi connect Cita &> /dev/null
-		} || {
-			log "trying Cita_Ext"
-			nmcli device wifi connect Cita_Ext &> /dev/null
-		} || {
-			err=$?
-			log "failed with status code ${err}"
-			exit "${err}"
-		}
+    log "connected"
 
-		log "connected"
-	fi
-	echo "---"
+    return 0
 }
 
-reconnect
+function test_connection() {
+    if ping -c 1 "${1}" &>/dev/null; then
+        log "test ${1} succesful"
+        return 0
+    else
+        log "test ${1} failed"
+        return 1
+    fi
+}
+
+function main() {
+    address_list=(
+        "1.1.1.1"
+        "google.com"
+        "100.100.100.100"
+    )
+
+    ctr=0
+    for address in "${address_list[@]}"; do
+        if test_connection "$address"; then
+            ((ctr++))
+        fi
+    done
+
+    if [[ ! $ctr -eq ${#address_list[@]} ]]; then
+        reconnect
+        sleep 30
+    fi
+}
+
+main
